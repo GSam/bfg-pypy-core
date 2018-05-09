4df435bce1b8abbb01e0893ffb035185990deacd
Garming Sam <garming_sam@outlook.com>
2018-05-09 08:10:22 +0000

    Add a bf to sbf converter

---
+++ convert_bf_to_sbf.py
@@ -0,0 +1,26 @@
+import sys
+import struct
+import uuid
+
+file1 = sys.argv[1]
+file2 = sys.argv[2]
+
+f1 = open(file1, 'rb')
+f2 = open(file2, 'wb')
+
+for b in f1.read():
+    if b == '\n':
+        continue
+    print b, 1
+    data = struct.pack(">Q", 0)
+    f2.write(data)
+    data = struct.pack(">Q", ord(b))
+    print repr(data)
+    f2.write(data)
+
+# id_uuid = uuid.uuid4().bytes
+# id_uuid = uuid.UUID(int=uuid.uuid4().int | (1 << 127)).bytes
+f2.write(id_uuid)
+
+f2.close()
+f1.close()
